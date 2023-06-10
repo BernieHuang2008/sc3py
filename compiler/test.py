61e8d0f242ed65eb50f45b339bb7930b8a5c032d
berniehuang2008 <berniehuang2008@163.com>
2023-06-10 23:19:54 +0800

    Update

--- compiler/test.py
+++ compiler/test.py
@@ -1,95 +1,135 @@
 import json
 
+# global variables
 sprites = {}  # {sprite_name: sprite} The dict contains all the Sprites in this program.
-res_file = open('result/main.py', 'w')  # The file to save the result.
-with open("project.json") as f:  # Load the 'project.json' file.
-    j = json.load(f)
-    for sprite in j['targets']:  # Get all the Sprites in this program.
-        name = sprite['name']  # Get the name of the Sprite.
-        sprites[name] = sprite  # Storage the Sprite in the 'sprites'.
+res_file = open('result/main.py', 'w')  # Compiled Python code will be written to this file.
 
-with open(
-        "blocks_format.compiler.config.json") as f:  # load the format file. It defined how to turn Scratch code into Python code.
+# open Scratch Code File
+with open("project.json") as f:
+    j = json.load(f)
+    # Get all Sprites in this program, and store them in 'sprites'.
+    for sprite in j['targets']:
+        name = sprite['name']
+        sprites[name] = sprite
+
+# load format file (which is used to turn Scratch into Py Code)
+with open("blocks_format.compiler.config.json") as f:
     FORMAT = json.load(f)  # Load to 'FORMAT'
 
 
-def get_blocks(sprite):
-    # Get all the blocks of the Sprite.
+def parse_sprite(sprite):
+    """
+    Parse a Sprite.
+    :param sprite: a sprite from 'sprites'.
+    :return: None. The result will be written to 'res_file' directly.
+    """
+
     def parse_each_block(block_id):
-        # Parse each block.
+        """
+        Parse each block.
+        :param block_id: the id of specified block (str) | a value (list)
+        :return: the parsed code (or value) as str.
+        """
 
-        def get_value(val):
-            if val.isdigit():
-                return val
-            else:
-                return "'{}'".format(val)
-
+        # if the block is a value, parse it.
         if isinstance(block_id, list):
-            if len(block_id) == 2:
-                return get_value(block_id[1])
-            elif len(block_id) == 3:
-                return "game.var({}, {})".format(get_value(block_id[1]), get_value(block_id[2]))
-            else:
-                print('Error: block_id is a list, but the length is not 2 or 3.', block_id)
-                return ''
-
-        block = blocks[block_id]  # Get the block.
-        opcode = block['opcode']  # Get 'opcode'. (The 'opcode' is the type of the block.)
-
-        _format = FORMAT[opcode]  # Get format of the specified opcode.
-        code = _format['code']  # Get scratch code.
-        var_counts = code.count(
-            '__!')  # Get the number of variables (which will be replaced by compiler) in the scratch code.
-        for i in range(var_counts):
-            v_index = code.find('__!')  # Index of the variable. VAR is started by '__!' and ended by '!__'.
-            v_str = code[v_index + 3:]  # The string of the variable.
-            v_str = v_str[:v_str.find("!__")]  # Ending by '!__'.
-            v_type = v_str.split('.')[0]  # Get the type of the variable. ('getval' or 'getcode' etc.)
-            v_content = '.'.join(v_str.split('.')[1:])  # This is the path shows where to get the content.
-            indent = ' ' * _format['indents'][i]  # Indent of this variable.
-            if v_type == 'getval':
-                # If the type is 'getval', then get the value of the variable and fill the blank directly (adding indent).
-                code = code.replace('__!' + v_str + '!__',
-                                    eval("block['fields']" + v_content).replace('\n', '\n' + indent),
-                                    1)  # replace only one times.
-            elif v_type == 'getcode':
-                # If the type is 'getcode', then get the code of the variable and parse it. Fill the blank with the parsed code (adding indent).
-                code = code.replace('__!' + v_str + '!__',
-                                    parse_each_block(eval("block['inputs']" + v_content)).replace('\n', '\n' + indent),
-                                    1)
-
-        if block['next'] is None:
-            # if the block is the last block, just return the code.
-            return code
+            # https://en.scratch-wiki.info/wiki/Scratch_File_Format
+            return {
+                4: lambda x: x[0],  # Number
+                5: lambda x: x[0],  # Positive Number
+                6: lambda x: x[0],  # Positive Integer
+                7: lambda x: x[0],  # Integer
+                8: lambda x: x[0],  # Angle
+                9: lambda x: "'{}'".format(x[0]),  # Color
+                10: lambda x: "'{}'".format(x[0]),  # String
+                11: lambda x: "game.broadcast({})".format(x[0]),  # Broadcast
+                12: lambda x: "game.var('{}', '{}')".format(x[0], x[1]),  # Variable
+                13: lambda x: "game.list('{}', '{}')".format(x[0], x[1]),  # List
+            }[block_id[0]](block_id[1:])  # just like 'switch'.
         else:
-            # else, parse the next block and return the code.
-            code += parse_each_block(block['next'])
-            return code
+            # get the block dict and some info.
+            block = blocks[block_id]
+            opcode = block['opcode']
 
-    blocks = sprite['blocks']  # Get all the blocks of the Sprite.
-    head_blocks = []  # The blocks which are the first block of the script.
-    for b_id in blocks:  # b_id is the id of each block.
-        block = blocks[b_id]  # Get the block.
-        b_toplevel = block["topLevel"]  # If the block is the first block of the script, then 'topLevel' is True.
-        if b_toplevel:
-            # If the block is a head_block, then add it to 'head_blocks'.
+            # get format of the specified 'opcode', and init.
+            _format = FORMAT[opcode]
+            code = _format['code']
+
+            # get the number of blanks in format.
+            var_counts = code.count('__!')
+
+            # fill the blanks one by one.
+            # Blanks is started by '__!' and ended by '!__'.
+            for i in range(var_counts):
+                # get info
+                v_index = code.find('__!')  # index
+                v_str = code[v_index + 3:]
+                v_str = v_str[:v_str.find("!__")]  # string value
+                v_type = v_str.split('.')[0]  # type
+                v_path = '.'.join(v_str.split('.')[1:])  # path
+                indent = ' ' * _format['indents'][i]  # indent
+
+                # fill the blank
+                if v_type == 'fields':
+                    # find the target from 'fields/v_path' and fill the blank
+                    code = code.replace('__!' + v_str + '!__',  # find blank
+                                        eval("block['fields']" + v_path).replace('\n', '\n' + indent),  # add indent
+                                        1)  # replace only one times.
+                elif v_type == 'inputs':
+                    # find the target from 'inputs/v_path' and fill the blank
+                    code = code.replace('__!' + v_str + '!__',
+                                        parse_each_block(eval("block['inputs']" + v_path)).replace('\n', '\n' + indent),
+                                        1)
+                else:
+                    pass
+
+            if block['next'] is None:
+                # if the block is the last block, just return the code.
+                return code
+            else:
+                # else, parse the next block and return the code.
+                code += parse_each_block(block['next'])
+                return code
+
+    # get all blocks
+    blocks = sprite['blocks']
+    head_blocks = []  # Blocks which are the first block of the script.
+
+    # get head_blocks
+    for b_id in blocks:  # b_id for 'block_id'.
+        block = blocks[b_id]
+        # check if it is a head block.
+        if 'when' in block['opcode']:
+            # add it to 'head_blocks'.
             head_blocks.append(block)
 
+    # generate code
     class_code_head = "class Generate_{}(scgame.Sprite):\n".format(sprite['name'].replace(' ', '_'))
     class_code = ""
-    active_condition_count = {}
-    for hb in head_blocks:  # hb stands for head_block.
-        active_condition = hb['opcode']  # Get the active condition of the script.
+    active_condition_count = {}  # The dict contains the count of each active condition.
+    for hb in head_blocks:  # hb for 'head_block'.
+        active_condition = hb['opcode']
         active_condition_count[active_condition] = active_condition_count.get(active_condition, 0) + 1
-        _next = hb['next']  # Get the next block's ID of the script.
-        code = parse_each_block(_next)  # Parse the next block, the code will be returned.
+        # parse the second block
+        _next = hb['next']
+        code = parse_each_block(_next)
+        # generate code
         class_code += '\ndef {}_{}(self):\n    {}'.format(active_condition,
                                                           active_condition_count[active_condition],
                                                           code.replace('\n', '\n    ')
-                                                          )  # Print the code.
+                                                          )
 
+    # generate event func.
+    for condition in active_condition_count:
+        class_code += '\n\ndef {}(self):\n    {}\n\n\n'.format(condition, '\n    '.join(
+            ["threading.Thread(target=self.{}_{})".format(condition, i + 1) for i in
+             range(active_condition_count[condition])]))
+
+    # write to file
     res_file.write(class_code_head + class_code.replace('\n', '\n    '))
 
 
+
+
 if __name__ == '__main__':
-    get_blocks(sprites['角色1'])
+    parse_sprite(sprites['角色1'])

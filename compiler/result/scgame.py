37ca427833d705e4b3bcfc17908a79407be9acf9
berniehuang2008 <berniehuang2008@163.com>
2023-06-22 09:27:20 +0800

    functiong supports adding. btw, I've made a TODO list

--- compiler/result/scgame.py
+++ compiler/result/scgame.py
@@ -1,23 +1,41 @@
 """
 It's the document of scgame.
 """
+import math
+import time
+
+
+def deg2rad(deg):
+    """
+    [Tool]: Convert degree to radian.
+    :param deg: degree
+    :return: radian
+    """
+    return deg / 180 * math.pi
 
 
 class Sprite:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.costume = None
         self.size = 100
         self.direction = 90
         self.volume = 100
         self.layer = game.get_layer(self)
         self.effects = {
             'COLOR': 0,
+            'BRIGHTNESS': 0,
+            'TRANSPARENCY': 0,
             'PITCH': 100
         }
+        self.backup = dict()
 
     def __str__(self):
         return self.__class__.__name__
+
+    def draw(self):  # TODO: function draw
+        pass
 
     # PART1: motion
     def move_forward(self, distance):
@@ -26,7 +44,11 @@ class Sprite:
         :param distance: distance to move
         :return: None
         """
-        pass
+        delta_x = distance * math.sin(deg2rad(self.direction))
+        delta_y = distance * math.cos(deg2rad(self.direction))
+        self.x += delta_x
+        self.y += delta_y
+        self.draw()
 
     def turn(self, angle):
         """
@@ -34,7 +56,9 @@ class Sprite:
         :param angle: angle to turn
         :return: None
         """
-        pass
+        self.direction += angle
+        self.direction %= 360
+        self.draw()
 
     def go_to(self, *args, secs=0):
         """
@@ -42,17 +66,29 @@ class Sprite:
         :param secs: time to glide, default 0s.
         :return: None
         """
+        target = [self.x, self.y]
         if len(args) == 2:
             # go to (x, y)
-            x, y = args
-            pass
+            target = tuple(args)
         elif len(args) == 1:
             # go somewhere
             target = args[0]
-            pass
-        pass
+            ...  # TODO: target finding
 
-    def point_towards(self, target):
+        if secs:  # if glide is on:
+            frame_num = math.ceil(secs / game.fps)
+            delta_x = target[0]
+            delta_y = target[1]
+            for _ in range(frame_num):
+                self.x += delta_x / frame_num
+                self.y += delta_y / frame_num
+                self.draw()
+                time.sleep(1 / game.fps)
+        else:
+            self.x, self.y = target
+            self.draw()
+
+    def point_towards(self, target):  # TODO: function point_towards
         """
         Point towards.
         :param target: target/direction to point
@@ -60,21 +96,27 @@ class Sprite:
         """
         pass
 
-    def on_edge(self):
+    def on_edge(self):  # TODO: function on_edge
         """
         Check if the sprite is on edge.
-        :return: bool
+        :return: which edge the sprite is on. ('left', 'right', 'top' or 'bottom')
         """
         pass
 
-    def bounce(self):
+    def bounce(self, mode):
         """
         Bounce.
+        :param mode: mode to bounce. ('left', 'right', 'top' or 'bottom')
         :return: None
         """
-        pass
+        if mode in ['left', 'right']:
+            self.direction = - self.direction
+        elif mode in ['top', 'bottom']:
+            self.direction = 180 - self.direction
+        self.direction %= 360
+        self.draw()
 
-    def restrict(self, attribute, style):
+    def restrict(self, attribute, style):  # TODO: function restrict
         """
         Restrict.
         :param attribute: attribute to restrict
@@ -84,7 +126,7 @@ class Sprite:
         pass
 
     # PART2: looks
-    def say(self, content, secs=0):
+    def say(self, content, secs=0):  # TODO: function say
         """
         Say something in seconds.
         :param content: content to say
@@ -93,7 +135,7 @@ class Sprite:
         """
         pass
 
-    def think(self, content, secs=0):
+    def think(self, content, secs=0):  # TODO: function think
         """
         Think something in seconds.
         :param content: content to think
@@ -108,14 +150,18 @@ class Sprite:
         :param costume: costume name to set
         :return: None
         """
-        pass
+        self.costume = costume
+        self.draw()
 
     def next_costume(self):
         """
         Switch to next costume.
         :return: None
         """
-        pass
+        self.costume_idx = self.costumes.index(self.costume) + 1
+        self.costume_idx %= len(self.costumes)
+        self.costume = self.costumes[self.costume_idx]
+        self.draw()
 
     def resize(self, size):
         """
@@ -123,16 +169,17 @@ class Sprite:
         :param size: size to resize (%)
         :return: None
         """
-        pass
+        self.size = size
+        self.draw()
 
-    def affect(self):
+    def affect(self):  # TODO: function affect
         """
         Apply all effects on this sprite.
         :return: None
         """
         pass
 
-    def clear_effects(self, *effects):
+    def clear_effects(self, *effects):  # TODO: function clear_effects
         """
         Clear effects of this sprite.
         :param effects: effect names to be clear
@@ -145,17 +192,20 @@ class Sprite:
         Show this sprite.
         :return: None
         """
-        pass
+        self.effects['TRANSPARENCY'] = self.backup.get('TRANSPARENCY', 100)
+        self.draw()
 
     def hide(self):
         """
         Hide this sprite.
         :return: None
         """
-        pass
+        self.backup['TRANSPARENCY'] = self.effects['TRANSPARENCY']
+        self.effects['TRANSPARENCY'] = 0
+        self.draw()
 
     # PART3: sound
-    def play_sound(self, sound, wait=False):
+    def play_sound(self, sound, wait=False):  # TODO: function play_sound
         """
         Play sound.
         :param sound: sound name
